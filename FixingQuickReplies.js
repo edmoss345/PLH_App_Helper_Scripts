4a40ba236d499b54e2fec599bd63e163ab70936e
Ed Moss <edmoss94@gmail.com>
2021-09-27 07:57:21 -0300

    v2

---
+++ FixingQuickReplies.js
@@ -0,0 +1,81 @@
+ 
+// Include fs module used to import a JSON file
+const { debug } = require('console');
+var fs = require('fs');
+   
+// Calling the readFileSync() method to read json file
+var json_string = fs.readFileSync('./em-playground.json').toString();
+// Convert to javascript object to work with
+var object = JSON.parse(json_string);
+        
+// Drill down to find the quick replies
+for (const flow of object.flows) {
+    for (const node of flow.nodes) {
+        for (const action of node.actions) {
+            // Check if there are quick replies in this action
+            try {
+                if(action.quick_replies.length > 0){
+                    // Quick replies found, store in an array to then work with
+                    const arr = action.quick_replies;
+                    console.log(arr)
+                    console.log(CreateUniqueQuickReplies(arr));              
+                    }
+                }            
+            catch(err) {
+                continue
+            }
+        }
+    }
+}
+
+function CreateUniqueQuickReplies(arr) {
+    var UniqueQuickReplies = []
+    var counter = 1
+    UniqueWords = FindUniqueWords(arr)
+
+    for (const replies of arr){
+        var NewReply = counter.toString();
+        const SplitReplies = replies.split(" ");
+        for (const replyword of SplitReplies){
+            if (CountIf(replyword,UniqueWords) == 1){
+                NewReply += " "
+                NewReply += replyword
+            }
+        }
+        UniqueQuickReplies.push(NewReply)
+        counter++
+    }
+return UniqueQuickReplies
+}
+
+
+function FindUniqueWords(arr) {
+    var AllWords = [];
+    var UniqueWords = [];
+    for (const replies of arr){
+        const SplitReplies = replies.split(" ")
+        for (const replyword of SplitReplies){
+            AllWords.push(replyword)
+        }
+    }
+    for (const word of AllWords){
+        if(CountIf(word, AllWords) == 1){
+            UniqueWords.push(word)
+        }
+    }
+    return UniqueWords
+}
+
+
+function CountIf(string, arr) {
+    var counter = 0
+    for (const member of arr){
+        if (string == member){
+            counter++
+        }
+    }
+    return counter
+}
+
+
+
